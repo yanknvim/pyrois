eb4a42416205d6761f366119c0bbe5d7d0878b7c
yanknvim <yanknvim@gmail.com>
2026-07-18 10:29:42 +0900

    first commit: impl simple fetch

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            veryl
+
+            gcc
+            pkgsCross.riscv32-embedded.buildPackages.gcc
+
+            verilator
+            gtkwave
+          ];
+        };
+      }
+    );
+}
